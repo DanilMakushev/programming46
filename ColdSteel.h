3e7a8da6524d1d09a43f8cc194f7f98e959e17a1
danil <danil.mak.206@gmail.com>
2026-06-06 00:03:56 +0700

    bug fix

--- ColdSteel.h
+++ ColdSteel.h
@@ -2,14 +2,16 @@
 #include "AbstractWeapon.h"
 #include <string>
 
-
 class ColdSteel : public AbstractWeapon {
 private:
     double _bladeLength;
 
-    
 public:
     ColdSteel(const std::string& name, double price, double weight, double bladeLength)
-        : AbstractWeapon(name, price, weight), _bladeLength(bladeLength) {}
+        : AbstractWeapon(name, price, weight) 
+    {
+        _bladeLength = (bladeLength < 0.0) ? 0.0 : bladeLength;
+    }
+
     double GetBladeLength() const { return _bladeLength; }
 };
